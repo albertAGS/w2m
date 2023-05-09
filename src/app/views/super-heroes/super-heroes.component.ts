bee46bc983eb97be2f1ad35043d909228b2926d6
albert <albert.griera.salvado@outlook.com>
2023-05-09 18:37:56 +0200

    add table to visualize heroes

--- src/app/views/super-heroes/super-heroes.component.ts
+++ src/app/views/super-heroes/super-heroes.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { SuperHeroes } from './super-heroes.interface';
 import { SuperHeroesService } from './super-heroes.service';
 
 @Component({
@@ -6,14 +9,39 @@ import { SuperHeroesService } from './super-heroes.service';
   templateUrl: './super-heroes.component.html',
   styleUrls: ['./super-heroes.component.scss'],
 })
-export class SuperHeroesComponent implements OnInit {
+export class SuperHeroesComponent implements OnInit, AfterViewInit {
   constructor(private superHeroesService: SuperHeroesService) {}
-
-  ngOnInit() {}
-
-  getHero(id: number) {
-    this.superHeroesService.getHeroById(id).subscribe((data: any) => {
-      console.log(data);
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  displayedColumns: string[] = [
+    'name',
+    'publisher',
+    'alter_ego',
+    'first_appearance',
+  ];
+  dataSource: MatTableDataSource<SuperHeroes>;
+  nameFiltered = '';
+  ngOnInit() {
+    this.superHeroesService.getAllHeros().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+      this.updatePaginator();
     });
   }
+
+  ngAfterViewInit() {
+    this.updatePaginator();
+  }
+  updatePaginator() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  // getHero(id: number) {
+  //   this.superHeroesService.getHeroById(id).subscribe((data: any) => {
+  //     console.log(data);
+  //   });
+  // }
 }
--- src/app/views/super-heroes/super-heroes.module.ts
+++ src/app/views/super-heroes/super-heroes.module.ts
@@ -1,5 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
 import { RouterModule, Routes } from '@angular/router';
 import { SuperHeroesComponent } from './super-heroes.component';
 
@@ -12,6 +17,14 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [SuperHeroesComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+  ],
 })
 export class SuperHeroesModule {}
